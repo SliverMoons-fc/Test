e5da7199983c4cd3bd7abef1f56cf2c2a86ea49f
fanchong3 <fanchong3@jd.com>
2020-07-29 11:43:35 +0800

    dev

--- src/main/java/com/test/service/MyService.java
+++ src/main/java/com/test/service/MyService.java
@@ -5,7 +5,7 @@ public class MyService {
     public void get(){
         System.out.println("1111");
         System.out.println("11111111111111111111111111111111111");
-        System.out.println("22222222222222222221`2");
+        System.out.println("222222222222222");
 
         System.out.println("1111");
         System.out.println("1111");
