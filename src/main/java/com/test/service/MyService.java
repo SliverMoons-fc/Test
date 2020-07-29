2140086afa7412e19b03b3909139b38f21bbdc8e
fanchong3 <fanchong3@jd.com>
2020-07-29 11:23:17 +0800

    测试提交

--- src/main/java/com/test/service/MyService.java
+++ src/main/java/com/test/service/MyService.java
@@ -4,5 +4,10 @@ package com.test.service;
 public class MyService {
     public void get(){
         System.out.println("1111");
+
+
+        System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("1111");
     }
 }
